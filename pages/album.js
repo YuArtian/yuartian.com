8ffeb31f466fa364c96dfa662f2c719b8eb8ccc3
zhaoxueyu <312364705@qq.com>
2019-04-03 14:40:28 +0800

    删除artdefault。。/ 新增 Ablum 入口

---
+++ pages/album.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: XueYu 😊
+ * @Date: 2019-04-03 14:37:05
+ * @Last Modified by: XueYu 😊
+ * @Last Modified time: 2019-04-03 14:38:46
+ */
+import React, { PureComponent } from 'react'
+
+class Ablum extends PureComponent {
+  render(){
+    return (
+      <div>Ablum</div>
+    )
+  }
+}
+
+export default Ablum
